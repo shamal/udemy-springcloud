bfcd7422d1df5b57a094e4d18f49bbc0dbf62494
Shamal Karunarathne <shamalk@gmail.com>
2018-10-26 15:36:35 +0800

    Added lab3 server, lab3 client and initial lab4 eureka

---
+++ lab-4-eureka-server/src/main/java/com/shamalk/rnd/springcloud/lab4eurekaserver/Lab4EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.shamalk.rnd.springcloud.lab4eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+}
